d75147b2687407184793542698f74f784e3a4078
Leandro Costa <leandrohernandes95@gmail.com>
2019-01-08 20:14:29 -0200

    create header component.

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./style.css";
+
+const Header = () => (
+  <header className="header">
+    <h1 className="header__logo">Star Wars</h1>
+  </header>
+);
+
+export default Header;
