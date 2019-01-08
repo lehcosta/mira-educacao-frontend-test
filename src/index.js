b9187170b267184481e63feadb667cc50cc4e216
Leandro Costa <leandrohernandes95@gmail.com>
2019-01-08 20:14:07 -0200

    add global style.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 
+import "./shared/theme.css";
+
 import App from "./app";
 
 ReactDOM.render(<App />, document.getElementById("root"));
